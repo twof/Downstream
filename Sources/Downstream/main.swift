33341cda16699fef42691300d93824d43185e799
Alex Reilly <areilly@yelp.com>
2020-10-23 02:06:21 -0700

    check for valid yaml and fail out if it's invalid

--- Sources/Downstream/main.swift
+++ Sources/Downstream/main.swift
@@ -17,16 +17,23 @@ let todos = fileList.flatMap { filePath -> [String] in
   
 //  print("filePath", filePath)
 //  print("parent", changedFile.parent?.path)
+//  print("yaml", try? changedFile.parent?.file(named: "downstream.yml").read())
+//  let associationsData = (try? changedFile.parent?.file(named: "downstream.yml").read() as? Data).flatMap { try? decoder.decode(AssociationsFile.self, from: $0) }
+//  print("associations", associationsData)
+  
   
   if
     let parent = changedFile.parent,
-    let downsteamYML = try? parent.file(named: "downstream.yml").read(),
-    let associationsFile = try? decoder.decode(AssociationsFile.self, from: downsteamYML)
+    let downsteamYML = try? parent.file(named: "downstream.yml").read()
   {
+    guard let associationsFile = try? decoder.decode(AssociationsFile.self, from: downsteamYML) else {
+      print("\(parent.path)downstream.yml could not be parsed")
+      exit(1)
+    }
     let fileName = changedFile.name
     let newTodos = associationsFile.associations[fileName] ?? []
     return newTodos.map {
-      "Our records indicate that you may need to update the docs at \($0) because changes were made to \(fileName)"
+      "Our records indicate that you may need to update the docs at \($0) because changes were made to  \(fileName)"
     }
   }
   
@@ -38,3 +45,5 @@ if !todos.isEmpty {
     print($0)
   }
 }
+
+exit(0)
